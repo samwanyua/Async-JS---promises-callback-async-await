8c2aee2f1b12ceeeccd4c96c679243b8e7ad4610
samwanyua <samexample8@gmail.com>
2024-01-09 06:58:19 +0300

    Callback

--- callBack.js
+++ callBack.js
@@ -1,4 +1,4 @@
-const post =[
+const posts =[
     {title: 'Post One', body: 'This is post one'},
     {title: 'Post Two', body: 'This is post two'}
 ];
@@ -6,10 +6,20 @@ const post =[
 function getPost(){
     setTimeout(() => {
         let output = '';
-        post.forEach((post, index)=> {
-            output+= `<li>${post.title}</li>`
+        posts.forEach((post, index)=> {
+            output += `<li>${post.title}</li>`
         });
         document.body.innerHTML = output;
-    }, 1000);
+    }, 1000); //time in milliseconds
 }
-getPost();
+
+function createPost(post, callback){
+    setTimeout(()=>{
+        posts.push(post); //post to the new array the new post
+        callback();
+    },2000);
+}
+
+
+
+createPost( {title: 'Post Three', body: 'This is post three'}, getPost);
